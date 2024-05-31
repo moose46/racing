bb004ea7297ac03620d545667c5b449afa36ceea
me <me@robertcurtiss.com>
2024-05-30 20:19:10 -0400

    Working Database Updates

--- nascar/admin.py
+++ nascar/admin.py
@@ -1,3 +1,39 @@
+from atexit import register
+
 from django.contrib import admin
+from django.utils.html import format_html
+
+from nascar.models import Driver, Race, RaceDriver, Team, Track
 
 # Register your models here.
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ["name", "show_website"]
+
+    def show_website(self, instance):
+        return format_html("<a href='{url}'>{url}</a>", url=instance.website)
+
+
+@admin.register(Race)
+class RaceAdmin(admin.ModelAdmin):
+    list_display = ["name", "track"]
+
+
+@admin.register(Driver)
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(RaceDriver)
+class RaceDriversAdmin(admin.ModelAdmin):
+    list_display = ["race", "driver", "track_name", "race_date"]
+    list_filter = ["race_id"]
+
+    def track_name(self, instance):
+        return instance.race.track  # "Peoria Motor Speedway"
+
+    def race_date(self, instance):
+        return instance.race.race_date
--- nascar/models.py
+++ nascar/models.py
@@ -1,6 +1,9 @@
 from datetime import timezone
-from django.db import models
+
 from django.contrib.auth.models import User
+from django.db import models
+
+
 # Create your models here.
 class Base(models.Model):
     createdAt = models.DateTimeField("date created", auto_now_add=True, null=True)
@@ -10,20 +13,28 @@ class Base(models.Model):
     class Meta:
         abstract = True
 
+
 class Team(Base):
     name = models.CharField(max_length=32)
+    owner = models.CharField(max_length=32, null=True)
+    website = models.URLField(null=True)
+
     def __str__(self) -> str:
         return self.name
-    
+
+
 class Driver(Base):
     name = models.CharField(max_length=32, default="", null=False)
+    website = models.URLField(null=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
     # car_no = models.IntegerField(default=99)
     # sponsor = models.CharField(max_length=64, default="N/A")
     # make = models.CharField(max_length=32, default="N/A")
     # team = models.CharField(max_length=64, default="N/A")
     # salary = models.IntegerField(default=3000)
     # starting_position = models.IntegerField(default=0)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    # drivers = models.ManyToManyField(Driver)
+
     def __str__(self) -> str:
         return self.name
 
@@ -31,16 +42,31 @@ class Driver(Base):
         unique_together = ["name", "team"]
         ordering = ["name"]
 
+
 class Track(Base):
     name = models.CharField(max_length=32)
+    website = models.URLField(null=True)
+
     def __str__(self) -> str:
         return self.name
+
 
 class Race(Base):
     name = models.CharField(max_length=64)
     track = models.ForeignKey(Track, on_delete=models.CASCADE, null=True)
     race_date = models.DateTimeField(null=True)
-    drivers = models.ManyToManyField(Driver)
+    website = models.URLField(null=True)
+
     def __str__(self) -> str:
         return self.name
-    
+
+
+class RaceDriver(Base):
+    """One Race One Driver"""
+
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    manufacturer = models.CharField(max_length=32, default="N/A")
+    start_pos = models.IntegerField(default=-1)
+    finish_pos = models.IntegerField(default=-1)
+    car_no = models.IntegerField(default=-1)
